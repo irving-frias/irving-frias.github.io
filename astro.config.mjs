82d7450e750de8f0daabdc66137c9a5522866716
irving-frias <irvingfrias41@gmail.com>
2024-11-22 13:24:33 -0700

    Added changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://irving-frias.github.io/',
-  base: 'irving-frias.github.io',
+  site: 'https://irving-frias.github.io/', // URL completa del sitio
+  base: '/irving-frias.github.io/',       // Ruta base con "/" inicial
   integrations: [sitemap(), react()]
 });
